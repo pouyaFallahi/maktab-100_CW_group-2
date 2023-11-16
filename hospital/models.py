86ac44614ca84aecae163f9def56bb846922ee37
iman <imangholamiimi@gmail.com>
2023-11-16 10:46:20 +0330

    INIT | Initialize Project

---
+++ hospital/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Patient(models.Model):
+    pass
+
+
+class Doctor(models.Model):
+    pass
+
+
+class PatientHistory(models.Model):
+    pass
+
+
+class PatientBill(models.Model):
+    pass
+
+
+class Appointment(models.Model):
+    pass
+
+
+class HealthInsurance(models.Model):
+    pass
+
+
+class PatientHistory(models.Model):
+    pass
