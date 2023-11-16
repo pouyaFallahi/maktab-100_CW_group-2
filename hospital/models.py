16d3be5fe8de54b17add376a58c6248a25affd4d
Artin <aaturbo1669@gmail.com>
2023-11-16 12:26:11 +0330

    UPDATE | Hospital Models

--- hospital/models.py
+++ hospital/models.py
@@ -1,14 +1,35 @@
+import phone_field
 from django.db import models
+from phone_field import PhoneField
 
 
-# Create your models here.
+class UserModel(models.Model):
+    gender_choices = (('MALE', 'male'), ('FEMALE', 'female'), ('OTHER', 'other'))
+    first_name = models.CharField(max_length=250, null=False)
+    last_name = models.CharField(max_length=250, null=False)
+    gender = models.Choices(gender_choices)
+    birth_date = models.DateField(DATE_INPUT_FORMATS=['%d-%m-%Y'], null=False)
+    phone = PhoneField(null=False)
+    email = models.EmailField()
+    address = models.TextField(null=False)
+    national_id = models.CharField(null=False)
 
-class Patient(models.Model):
+    class Meta:
+        abstract = True
+
+
+class HealthInsurance(models.Model):
     pass
 
 
-class Doctor(models.Model):
-    pass
+class Patient(UserModel):
+    patient_id = models.IntegerField(primary_key=True, serialize=True)
+    health_insurance_id = models.ForeignKey(HealthInsurance, on_delete=models.CASCADE, related_name='patient')
+
+
+class Doctor(UserModel):
+    specialization = models.CharField(null=False)
+    medical_council_code = models.IntegerField(null=False)
 
 
 class PatientHistory(models.Model):
@@ -20,12 +41,7 @@ class PatientBill(models.Model):
 
 
 class Appointment(models.Model):
-    pass
-
-
-class HealthInsurance(models.Model):
-    pass
-
-
-class PatientHistory(models.Model):
-    pass
+    patient_id = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='appointment')
+    doctor_id = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='appointment')
+    date = models.DateTimeField(DATETIME_FORMAT="%Y-%m-%d  %H:%M:%S")
+    reason = models.TextField(null=False)
