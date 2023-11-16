19fd94fb274084ec83b7bfe51a5437b3fe01ef10
MrAsad1 <aaturbo1669@gmail.com>
2023-11-16 15:22:56 +0330

    UPDATE | Hospital Models

--- hospital/models.py
+++ hospital/models.py
@@ -1,6 +1,7 @@
 import phone_field
 from django.db import models
 from phone_field import PhoneField
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class UserModel(models.Model):
@@ -19,12 +20,16 @@ class UserModel(models.Model):
 
 
 class HealthInsurance(models.Model):
-    pass
+    company = models.CharField(null=False)
+    address = models.TextField(null=False)
+    phone = PhoneField(null=False)
+    email = models.EmailField(blank=True)
+    discount_percentage = models.FloatField(null=False)
 
 
 class Patient(UserModel):
     patient_id = models.IntegerField(primary_key=True, serialize=True)
-    health_insurance_id = models.ForeignKey(HealthInsurance, on_delete=models.CASCADE, related_name='patient')
+    health_insurance_id = models.ForeignKey(HealthInsurance, on_delete=models.CASCADE, related_name='patient', null=True)
 
 
 class Doctor(UserModel):
@@ -33,11 +38,19 @@ class Doctor(UserModel):
 
 
 class PatientHistory(models.Model):
-    pass
+    patient_id = models.OneToOneField(Patient, on_delete=models.CASCADE)
+    doctor_id = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    date = models.DateTimeField(DATETIME_FORMAT="%Y-%m-%d%H:%M:%S")
+    diagnosis = models.TextField(null=False)
+    prescription = models.TextField(null=False)
+    drug_used = models.TextField(blank=True)
+    note = models.TextField(blank=True)
+    patient_history_id = models.IntegerField()
 
 
 class PatientBill(models.Model):
-    pass
+    patient_bill_id = models
+    patient_history_id = models.OneToOneField(PatientHistory, on_delete=models.CASCADE)
 
 
 class Appointment(models.Model):
