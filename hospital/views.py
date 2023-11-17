1537c2e3f2b417c33dcbb8adf3d8d3979846f191
iman <imangholamiimi@gmail.com>
2023-11-17 18:18:16 +0330

    UPDATE | VIEWS

--- hospital/views.py
+++ hospital/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from .models import *
 from django.http import HttpResponse
+from .forms import DoctorForm
 
-def doctor(request):
-    doctor_obj = Doctor.objects.all()
+
+def doctor_list(request):
+    if request.method == 'POST':
+        form = DoctorForm(request.POST)
+        if form.is_valid():
+
+            print(type(doctor_obj))
+
+
+    else:
+        form = DoctorForm()
+        context = {
+            'form': form
+        }
+    return render(request, 'hospital/doctor.html', context)
+
+
+def patient_list(request):
     patient_list = Appointment.objects.all()
-    return HttpResponse(';ldfgkj[weqfogiklnhj')
-
-
-def add_patient(request):
     pass
 
 
-def patient(request):
+def add_user(request):
     pass
