2f6107ae03cb5b0d880d8f40151c9ce1721113ba
ImanGholami <imangholamiimi@gmail.com>
2023-11-18 20:20:53 +0330

    UPDATE | URLS

--- hospital/urls.py
+++ hospital/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 from django.urls import path
-from .views import doctor_list, patient_list
+from .views import doctor_list, patient_list, home
 
+app_name = 'hospital'
 urlpatterns = [
+    path('main/', home, name='home'),
     path("doctor/", doctor_list, name='doctor'),
     path('patient/', patient_list, name='patient')
 ]
